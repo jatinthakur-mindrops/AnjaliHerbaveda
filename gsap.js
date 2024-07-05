847ef9fa21f561562fd09141877086ca9ad611a0
jatin thakur <jt872678@gmail.com>
2024-07-05 16:42:18 +0530

    initial commit

---
+++ gsap.js
@@ -0,0 +1,14 @@
+gsap.from(".nav-left , .nav-right , .navimg",{
+    y:-10,
+    ducration:2,
+    delay:1,
+    opacity:0,
+    stagger:0.3
+})
+// gsap.from(".topbannerimg",{
+//     y:-10,
+//     ducration:2,
+//     delay:1,
+//     opacity:0,
+//     stagger:0.3
+// })
