e9ba0c748763602c4cafd93b3456e72e8f8784b9
jatin thakur <jt872678@gmail.com>
2024-07-08 18:29:59 +0530

    create new sections

--- gsap.js
+++ gsap.js
@@ -12,3 +12,18 @@ gsap.from(".nav-left , .nav-right , .navimg",{
 //     opacity:0,
 //     stagger:0.3
 // })
+
+
+gsap.from(".top",{
+    opacity:0,
+    duration:1
+})
+// gsap.from(".card-group .gif-card",{
+//     scale:0,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:".card-group .gif-card",
+//         scroller:"body",
+//         markers:true,
+//     }
+// })
